0541d7ccd92cd58fb914116bbc66e4de447e4657
Jiheon Kim <siwmua0@naver.com>
2024-01-17 17:37:37 +0900

    msw 2.0 버전에서 msw/node 충돌로인해 jest에서 API모킹은 보류

--- setupTests.ts
+++ setupTests.ts
@@ -1,2 +1,16 @@
 import "@testing-library/react";
 import "@testing-library/jest-dom";
+
+// import server from "@/mocks/server";
+
+// beforeAll(() => {
+//   server.listen();
+// });
+
+// afterEach(() => {
+//   server.resetHandlers();
+// });
+
+// afterAll(() => {
+//   server.close();
+// });
